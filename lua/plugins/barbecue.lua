7439c44f212afb426ba184db5bfcf047c429d1af
wangrui2 <wangrui2@tci-sz.com.cn>
2024-10-17 16:06:53 +0800

    add barbecue plugin

---
+++ lua/plugins/barbecue.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      -- configurations go here
+    },
+    config = function()
+      require('barbecue').setup({
+        options = {
+          theme = 'tokyonight'
+        },
+      })
+    end
+  }
+}
