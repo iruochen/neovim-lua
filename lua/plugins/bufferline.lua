1b44110c83322cc64ea4d40a5af1a123807195c3
wangrui2 <wangrui2@tci-sz.com.cn>
2024-10-18 15:34:52 +0800

    modify bufferline config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,10 +2,19 @@ return {
   {
     "akinsho/bufferline.nvim",
     dependencies = {
-      "nvim-tree/nvim-web-devicons"
+      "nvim-tree/nvim-web-devicons",
+      'moll/vim-bbye'
     },
     config = function()
-      require("bufferline").setup()
+      require("bufferline").setup({
+        options = {
+          close_command = "Bdelete! %d",
+        },
+      })
+      -- 键位映射
+      vim.api.nvim_set_keymap("n", "<C-h>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<C-l>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<C-w>", ":Bdelete!<CR>", { noremap = true, silent = true })
     end
   }
 }
