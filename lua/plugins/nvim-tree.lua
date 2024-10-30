4b89c7c7e2f45b43b02c4fbccfc13873a1b30e79
wangrui2 <wangrui2@tci-sz.com.cn>
2024-10-30 14:49:57 +0800

    modify nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,16 +1,14 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup({
-      view = {
-        width = 30
-      }
-    })
-    vim.api.nvim_set_keymap('n', 'tt', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-  end,
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require('neo-tree').setup()
+      vim.keymap.set({"n", "v"}, "tt", [[<cmd>Neotree toggle<CR>]])
+    end
+  }
 }
