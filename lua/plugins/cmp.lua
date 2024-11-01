84ca4bf7b289924047fc40012daaf7ba73174577
若尘 <ruochen@ruochendeMacBook-Air.local>
2024-11-01 08:28:46 +0800

    cmp config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,8 +1,90 @@
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
-    "hrsh7th/cmp-path"
+    "hrsh7th/cmp-path",
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-cmdline',
+    {
+      'saadparwaiz1/cmp_luasnip',
+      dependencies = {
+        'L3MON4D3/LuaSnip',
+        dependencies = {
+          'rafamadriz/friendly-snippets'
+        }
+      },
+    },
   },
   config = function()
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    local luasnip = require('luasnip')
+    local cmp = require 'cmp'
+    cmp.setup {
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'path' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+      }),
+      mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if luasnip.expandable() then
+              luasnip.expand()
+            else
+              cmp.confirm({
+                select = true,
+              })
+            end
+          else
+            fallback()
+          end
+        end),
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      experimental = {
+        ghost_text = true
+      }
+    }
+
+    cmp.setup.cmdline('/', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' },
+      }
+    })
+
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' },
+        { name = 'cmdline' }
+      })
+    })
   end
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,7 +12,7 @@ return {
     -- 右下角显示lsp进度
     "j-hui/fidget.nvim",
     -- lsp ui美化
-    -- "nvimdev/lspsaga.nvim",
+    "nvimdev/lspsaga.nvim",
   },
   config = function()
     local servers = {
@@ -50,9 +50,12 @@ return {
       end
 
       nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-      nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-      nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-      nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+      -- nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+      nmap('gd', require "telescope.builtin".lsp_definitions, '[G]oto [D]efinition')
+      -- nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+      nmap('K', "<cmd>Lspsaga hover_doc<CR>", 'Hover Documentation')
+      -- nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+      nmap('gi', require "telescope.builtin".lsp_implementations, '[G]oto [I]mplementation')
       -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
       nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
       nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
@@ -60,27 +63,34 @@ return {
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
       end, '[W]orkspace [L]ist Folders')
       nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-      nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-      nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+      -- nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+      nmap('<leader>rn', "<cmd>Lspsaga rename ++project<cr>", '[R]e[n]ame')
+      -- nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+      nmap('<leader>ca', "<cmd>Lspsaga code_action<CR>", '[C]ode [A]ction')
       nmap('<leader>da', require "telescope.builtin".diagnostics, '[D]i[A]gnostics')
       nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
       -- nmap('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
       vmap("=", function()
         vim.lsp.buf.format { async = true }
       end, "[F]ormat code")
+      nmap("==", function()
+        vim.lsp.buf.format { async = true }
+      end, "[F]ormat code")
     end
     require("neodev").setup()
     require("fidget").setup({})
-    -- require("lspsaga").setup()
+    require("lspsaga").setup()
     require("mason").setup()
     require("mason-lspconfig").setup({
       ensure_installed = vim.tbl_keys(servers),
     })
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
     for server, config in pairs(servers) do
       require("lspconfig")[server].setup(
         vim.tbl_deep_extend("keep",
           {
-            on_attach = on_attach
+            on_attach = on_attach,
+            capabilities = capabilities
           },
           config
         )
